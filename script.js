9f691686adc3c067d4b7612df79aeb6e7219853f
Barak Almog <barak.almog@gmail.com>
2024-09-24 11:30:34 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const text = 'Hello World'
+const helloWorld = document.getElementById('hello-world')
+let index = 0
+
+function typeText() {
+  if (index < text.length) {
+    helloWorld.innerHTML += text.charAt(index)
+    index++
+    setTimeout(typeText, 150)
+  } else {
+    // Wait for 2 seconds before clearing and restarting
+    setTimeout(resetAndRestart, 2000)
+  }
+}
+
+function resetAndRestart() {
+  helloWorld.innerHTML = ''
+  index = 0
+  typeText()
+}
+
+window.onload = typeText
